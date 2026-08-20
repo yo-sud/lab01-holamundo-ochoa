f6c36e342b2e53d9576498ab51db319f9b11180b
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-08-19 22:34:16 -0500

    Se agregó un botón se le dió un color

--- app/src/main/java/com/ochoa/lab01holamundo/MainActivity.kt
+++ app/src/main/java/com/ochoa/lab01holamundo/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.ochoa.lab01holamundo
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.unit.sp
 import com.ochoa.lab01holamundo.ui.theme.Lab01HolaMundoTheme
 
@@ -37,6 +37,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         Text(text = "¡Hola, soy $name!", fontSize = 24.sp)
         Text(text = "Curso: Programación en Móviles")
+        Button(onClick = {
+        }) {
+            Text(text = "Presióname")
+        }
     }
 }
 
--- app/src/main/java/com/ochoa/lab01holamundo/ui/theme/Color.kt
+++ app/src/main/java/com/ochoa/lab01holamundo/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.ochoa.lab01holamundo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFF81C784)
+val PurpleGrey80 = Color(0xFFAED581)
+val Pink80 = Color(0xFFFFD54F)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF1B5E20)
+val PurpleGrey40 = Color(0xFF33691E)
+val Pink40 = Color(0xFFE65100)
