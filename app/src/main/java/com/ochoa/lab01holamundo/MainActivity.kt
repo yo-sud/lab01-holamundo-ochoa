140ec886c4f1d57bb7554545d9ef3e6d9fe8a70c
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-08-19 21:11:48 -0500

    Proyecto inicial: Hola Mundo con Jetpack Compose

---
+++ app/src/main/java/com/ochoa/lab01holamundo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ochoa.lab01holamundo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ochoa.lab01holamundo.ui.theme.Lab01HolaMundoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab01HolaMundoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab01HolaMundoTheme {
+        Greeting("Android")
+    }
+}
